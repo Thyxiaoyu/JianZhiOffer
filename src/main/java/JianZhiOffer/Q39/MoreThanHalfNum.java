20ec96bb2df28a7017961121735fd9e9a6e9171d
钟晓宇 <792097223@qq.com>
2019-05-01 15:03:49 +0800

    剑指Offer代码

--- src/main/java/JianZhiOffer/Q39/MoreThanHalfNum.java
+++ src/main/java/JianZhiOffer/Q39/MoreThanHalfNum.java
@@ -1,12 +1,44 @@
 package JianZhiOffer.Q39;
 
+import com.sun.org.apache.regexp.internal.RE;
+import jdk.nashorn.internal.ir.ReturnNode;
+
 /**
  * @author ZXY
  * @create 2019-04-03 17:20
  * 老来多健忘，唯不忘相思
  */
 public class MoreThanHalfNum {
-    public static int moreThanHalfNum(int[] nums) {
-return 1;
+    public static Integer moreThanHalfNum(int[] nums) {
+        if (nums == null || nums.length <= 0) return null;
+        int ans = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                ans = num;
+                count = 1;
+                continue;
+            }
+            if (ans == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        if (isMoreThanHalf(nums,ans)) {
+            return ans;
+        }
+        return null;
+    }
+
+
+    private static boolean isMoreThanHalf(int[] nums, int number) {
+        int times = 0;
+        for (int num : nums) {
+           if (num == number) {
+               times++;
+           }
+        }
+        return times * 2 > nums.length;
     }
 }
